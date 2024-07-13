d014953e016ad9560c5a4ba3b59e2c5c79b2fc80
adeleke-code <femiadeleke601@gmail.com>
2024-07-13 23:32:24 +0100

    team

--- accounts/urls.py
+++ accounts/urls.py
@@ -16,9 +16,9 @@ urlpatterns = [
     path('auth/token/', views.user_auth, name="login_view"),
     path("auth/logout/", views.logout_view, name="logout_view"),
     path('auth/verify/', views.otp_verification),
-    path('teams/', views.teams_view),
-    path('teams/user/join/', views.join_team),
-    path('teams/users/', views.user_teams),
+    path('teams/create/', views.teams_view),
+    path('teams/join/', views.join_team),
+    path('teams/list/', views.user_teams),
 
 
 ]
