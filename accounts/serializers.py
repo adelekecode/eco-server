22f55643c41724de2495f3b60fcf8322e0394906
adeleke-code <femiadeleke601@gmail.com>
2024-07-18 02:17:35 +0100

    ai server

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -144,6 +144,6 @@ class ScanCountSerializer(serializers.ModelSerializer):
 
 class ImageSerializer(serializers.Serializer):
 
-    image = serializers.CharField(max_length=None, required=True)
+    image = serializers.CharField(required=True)
 
 
